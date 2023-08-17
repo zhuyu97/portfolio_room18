b2d78d482b0823557ff48c07c61379f4f20363e4
zhuyu97 <15001839220@163.com>
2023-08-17 23:32:18 +0800

    front-end code

---
+++ Dump20230816.sql
@@ -0,0 +1,264 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: portfolio
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bond_detail`
+--
+
+DROP TABLE IF EXISTS `bond_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bond_detail` (
+  `bond_detail_id` bigint NOT NULL AUTO_INCREMENT,
+  `bond_id` bigint DEFAULT NULL,
+  `bond_price` decimal(10,2) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`bond_detail_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bond_detail`
+--
+
+LOCK TABLES `bond_detail` WRITE;
+/*!40000 ALTER TABLE `bond_detail` DISABLE KEYS */;
+INSERT INTO `bond_detail` VALUES (1,1,500.00,'2023-08-15 12:01:26'),(2,1,1000.00,'2023-08-16 02:13:44'),(3,1,1000.00,'2023-08-16 02:14:09'),(4,1,1200.00,'2023-08-16 02:14:57');
+/*!40000 ALTER TABLE `bond_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bonds`
+--
+
+DROP TABLE IF EXISTS `bonds`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bonds` (
+  `bond_id` bigint NOT NULL AUTO_INCREMENT,
+  `bond_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`bond_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bonds`
+--
+
+LOCK TABLES `bonds` WRITE;
+/*!40000 ALTER TABLE `bonds` DISABLE KEYS */;
+INSERT INTO `bonds` VALUES (1,'Photovoltaic_ETF');
+/*!40000 ALTER TABLE `bonds` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cash`
+--
+
+DROP TABLE IF EXISTS `cash`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cash` (
+  `cash_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint DEFAULT NULL,
+  `amount` decimal(20,2) DEFAULT NULL,
+  PRIMARY KEY (`cash_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cash`
+--
+
+LOCK TABLES `cash` WRITE;
+/*!40000 ALTER TABLE `cash` DISABLE KEYS */;
+INSERT INTO `cash` VALUES (1,1,500000.00),(2,2,99999.00);
+/*!40000 ALTER TABLE `cash` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hold_assets`
+--
+
+DROP TABLE IF EXISTS `hold_assets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hold_assets` (
+  `hold_assets_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint DEFAULT NULL,
+  `production_id` bigint DEFAULT NULL,
+  `production_type` int DEFAULT NULL,
+  `production_amount` bigint DEFAULT NULL,
+  `cost` decimal(20,2) DEFAULT '0.00',
+  PRIMARY KEY (`hold_assets_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hold_assets`
+--
+
+LOCK TABLES `hold_assets` WRITE;
+/*!40000 ALTER TABLE `hold_assets` DISABLE KEYS */;
+INSERT INTO `hold_assets` VALUES (1,1,1,0,2500,50000.00),(2,1,2,0,5000,10000.00),(3,1,1,1,500,200000.00);
+/*!40000 ALTER TABLE `hold_assets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `net_worth`
+--
+
+DROP TABLE IF EXISTS `net_worth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `net_worth` (
+  `net_worth_id` bigint NOT NULL AUTO_INCREMENT,
+  `net_worth_value` decimal(20,2) DEFAULT NULL,
+  `time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`net_worth_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `net_worth`
+--
+
+LOCK TABLES `net_worth` WRITE;
+/*!40000 ALTER TABLE `net_worth` DISABLE KEYS */;
+INSERT INTO `net_worth` VALUES (1,3610000.00,'2023-08-14 23:40:37'),(2,3610000.00,'2023-08-14 23:40:41'),(3,3610000.00,'2023-08-14 23:40:42'),(4,3610000.00,'2023-08-14 23:40:43'),(5,3610000.00,'2023-08-14 23:40:43'),(6,3610000.00,'2023-08-14 23:40:44'),(7,3610000.00,'2023-08-14 23:40:44'),(8,3610000.00,'2023-08-14 23:40:45'),(9,3610000.00,'2023-08-14 23:40:56'),(10,3610000.00,'2023-08-14 23:40:56'),(11,3610000.00,'2023-08-14 23:40:57'),(12,3610000.00,'2023-08-14 23:55:46'),(13,3610000.00,'2023-08-14 23:55:48'),(14,3610000.00,'2023-08-14 23:55:51'),(15,3610000.00,'2023-08-14 23:55:53'),(16,3610000.00,'2023-08-14 23:55:54'),(17,3610000.00,'2023-08-14 23:55:54'),(18,3610000.00,'2023-08-14 23:55:55'),(19,3610000.00,'2023-08-14 23:55:55');
+/*!40000 ALTER TABLE `net_worth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock_detail`
+--
+
+DROP TABLE IF EXISTS `stock_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stock_detail` (
+  `stock_detail_id` bigint NOT NULL AUTO_INCREMENT,
+  `stock_id` bigint DEFAULT NULL,
+  `stock_price` decimal(10,2) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`stock_detail_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock_detail`
+--
+
+LOCK TABLES `stock_detail` WRITE;
+/*!40000 ALTER TABLE `stock_detail` DISABLE KEYS */;
+INSERT INTO `stock_detail` VALUES (1,1,800.00,NULL),(2,1,800.00,'2023-08-14 13:01:28'),(3,1,800.00,'2023-08-14 13:07:16'),(4,1,800.00,'2023-08-14 13:19:27'),(5,2,100.00,'2023-08-14 13:20:27'),(6,1,800.00,'2023-08-15 04:01:26'),(7,1,800.00,'2023-08-15 10:23:05'),(8,1,800.00,'2023-08-15 10:24:48'),(9,1,800.00,'2023-08-15 10:24:48'),(10,2,100.00,'2023-08-14 13:20:27'),(11,1,800.00,'2023-08-15 10:24:48'),(12,2,100.00,'2023-08-14 13:20:27');
+/*!40000 ALTER TABLE `stock_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stocks`
+--
+
+DROP TABLE IF EXISTS `stocks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stocks` (
+  `stock_id` bigint NOT NULL AUTO_INCREMENT,
+  `stock_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`stock_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stocks`
+--
+
+LOCK TABLES `stocks` WRITE;
+/*!40000 ALTER TABLE `stocks` DISABLE KEYS */;
+INSERT INTO `stocks` VALUES (1,'XXX'),(2,'Meta');
+/*!40000 ALTER TABLE `stocks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction_record`
+--
+
+DROP TABLE IF EXISTS `transaction_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction_record` (
+  `tr_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint DEFAULT NULL,
+  `transaction_type` int DEFAULT NULL,
+  `number_of_transaction` bigint DEFAULT NULL,
+  `production_id` bigint DEFAULT NULL,
+  `production_type` int DEFAULT NULL,
+  `production_detail_id` bigint DEFAULT NULL,
+  `production_price` decimal(20,2) DEFAULT NULL,
+  `cost` decimal(20,2) DEFAULT NULL,
+  `remain_cash` decimal(20,2) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`tr_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction_record`
+--
+
+LOCK TABLES `transaction_record` WRITE;
+/*!40000 ALTER TABLE `transaction_record` DISABLE KEYS */;
+INSERT INTO `transaction_record` VALUES (1,1,0,1000,1,0,3,800.00,800000.00,991888777.00,'2023-08-14 13:19:27'),(2,1,1,500,1,0,4,600.00,300000.00,310000.00,'2023-08-15 04:01:26'),(3,1,0,500,1,0,1,500.00,250000.00,60000.00,'2023-08-15 04:20:58'),(4,1,1,500,1,0,6,999999.00,499999500.00,500059500.00,'2023-08-15 10:23:05'),(5,1,1,500,1,0,7,999999.00,499999500.00,1000059000.00,'2023-08-15 10:24:48');
+/*!40000 ALTER TABLE `transaction_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-16 11:20:35
